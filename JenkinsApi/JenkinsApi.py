b2e0e0cb6d05412801d2986b2c6dfcab08890f80
tuweizhi <tuweizhi@126.com>
2020-08-09 22:08:48 +0800

    jenkins-api  create-node

---
+++ JenkinsApi/JenkinsApi.py
@@ -0,0 +1,48 @@
+import jenkins
+import csv
+import sys
+import os
+
+def start_service():
+    j = jenkins.Jenkins('http://192.168.1.111:8080', username='jenkins',password='11q!!Q')
+    return j
+
+def create_node(name,hostIP,port):
+    j.create_node(
+        name,
+        numExecutors=1,
+        nodeDescription=None,
+        remoteFS='/home/build/jenkins',
+        labels=name,
+        exclusive=True,
+        launcher=jenkins.LAUNCHER_SSH,
+        launcher_params={
+            'port': port,
+            'credentialsId': '613873f3-f919-49cb-ac25-6c5855964c60',
+            'host': hostIP
+        }
+    )
+
+def csv_info():
+    with open('node_info.csv') as f:
+        csv_reader = csv.DictReader(f)
+        return [res for res in csv_reader]
+
+def create_nodes(list_info):
+    for i in list_info:
+        if not j.node_exists(i['name']):
+            create_node(i['name'], i['hostIP'], i['port'])
+            if j.node_exists(i['name']):
+                print('节点创建成功:%s' % i['name'])
+            else:
+                print('节点创建失败：%s' % i['name'])
+
+if __name__ == "__main__":
+    # 调用jenkins服务
+    j = start_service()
+    # 创建节点
+    # create_node(j,name,hostIP,port)
+    list_info = csv_info()
+    # 批量创建节点
+    create_nodes(list_info)
+    
---
+++ JenkinsApi/README.md
@@ -0,0 +1,9 @@
+# 使用jenkins接口模块踩坑记录
+
+1. 默认连通性不通，解决方法：
+    1） 修改jenkins master的全局安全设置
+        勾选匿名用户具有可读权限
+    2） 调用服务后先认证信息
+        get_whoami()
+
+2. create_job 指定的config_xml必须是 utf-8编码
